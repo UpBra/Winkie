9608541666c0de77f363abab6e52e3198d220dd7
Blair <blair.replogle@gmail.com>
2020-03-16 19:21:53 -0600

    Initial Commit

---
+++ Winkie/Winkie.h
@@ -0,0 +1,19 @@
+//
+//  Winkie.h
+//  Winkie
+//
+//  Created by Blair on 3/16/20.
+//  Copyright © 2020 gleesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Winkie.
+FOUNDATION_EXPORT double WinkieVersionNumber;
+
+//! Project version string for Winkie.
+FOUNDATION_EXPORT const unsigned char WinkieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Winkie/PublicHeader.h>
+
+
